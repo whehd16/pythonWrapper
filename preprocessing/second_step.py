7d6832490a3f66c03f25be8c4f50c46093ad48f1
Dong-Hee Cho <whehd16@naver.com>
2020-10-05 13:22:40 +0900

    20201005_add compare

--- preprocessing/second_step.py
+++ preprocessing/second_step.py
@@ -42,6 +42,29 @@ def cutByYear(csv_paths, cut_list):
             temp_csv = temp_csv.drop(delete_list, 0)
             temp_csv.to_csv('./second(before_preprocessing)/'+ str(csv_path.split('_')[1]) + '/'+str(folder)+'/'+str(detail)+'/'+str(start)+'_'+str(end)+'.csv', index = False)
 
+def cutByYear_test(csv_paths, cut_list):
+    for csv_path in csv_paths:
+        csv = pd.read_csv(csv_path)
+        for folder, detail, start, end in cut_list:
+            periods= [ year for year in range(start, end+1)]
+            temp_csv = copy.deepcopy(csv)
+            delete_list = []
+            for i in range(len(temp_csv)):
+                split_date = temp_csv['Date'][i].split('-')
+                if len(split_date) > 1:
+                    if int(split_date[0]) not in periods:
+                        delete_list.append(i)
+                else:
+                    split_date = temp_csv['Date'][i].split('.')
+                    if int(split_date[0]) not in periods:
+                        delete_list.append(i)
+            temp_csv = temp_csv.drop(delete_list, 0)
+            # print(temp_csv.columns)
+            for column in temp_csv.columns:
+                if column != 'Date' and column != 'KOSPI_Now':
+                    temp_csv = temp_csv.drop([column], 1)
+            temp_csv.to_csv('./fourth(compare)/'+ str(csv_path.split('_')[1]) + '/'+str(folder)+'/'+str(detail)+'/'+str(start)+'_'+str(end)+'.csv', index = False)
+
 def cutByYear_whole(csv_paths, cut_list):
     for csv_path in csv_paths:
         csv = pd.read_csv(csv_path)
@@ -130,40 +153,73 @@ def cutByYear_whole(csv_paths, cut_list):
 
 # ])
 
-cutByYear(['./주식_rate_1차_전처리.csv'],
+# cutByYear(['./주식_rate_1차_전처리.csv'],
+# [
+#     #여기는 학습 데이터     #여기는 테스트 데이터
+#     [4, 22, 2007, 2008],
+#     [4, 22, 2009, 2010],
+#     [4, 22, 2011, 2012],
+#     [4, 22, 2013, 2014],
+#     [4, 22, 2015, 2016],
+
+#     [4, 31, 2007, 2009],
+#     [4, 31, 2009, 2011],
+#     [4, 31, 2011, 2013],
+#     [4, 31, 2013, 2015],
+#     [4, 31, 2015, 2017],
+
+#     [6, 33, 2007, 2009],
+#     [6, 33, 2010, 2012],
+#     [6, 33, 2013, 2015],
+
+#     [6, 42, 2007, 2010],
+#     [6, 42, 2010, 2013],
+#     [6, 42, 2013, 2016],
+
+#     [8, 44, 2007, 2010],
+#     [8, 44, 2011, 2014],
+
+#     [8, 53, 2007, 2011],
+#     [8, 53, 2011, 2015]
+
+# ])
+
+# cutByYear_whole(['./주식_rate_1차_전처리.csv'],
+# [
+#     [2007, 2010], [2009, 2012], [2011, 2014], [2013, 2016], [2015, 2018],
+#     [2007, 2012], [2010, 2015], [2013, 2018],
+#     [2007, 2014], [2011, 2018]
+# ])
+
+
+###테스트 데이터 만들기 위함
+
+cutByYear_test(['./주식_index_1차_전처리.csv', './주식_delta_1차_전처리.csv', './주식_rate_1차_전처리.csv'],
 [
     #여기는 학습 데이터     #여기는 테스트 데이터
-    [4, 22, 2007, 2008],
-    [4, 22, 2009, 2010],
-    [4, 22, 2011, 2012],
-    [4, 22, 2013, 2014],
-    [4, 22, 2015, 2016],
+    [4, '22', 2009, 2010],
+    [4, '22', 2011, 2012],
+    [4, '22', 2013, 2014],
+    [4, '22', 2015, 2016],
+    [4, '22', 2017, 2018],
 
-    [4, 31, 2007, 2009],
-    [4, 31, 2009, 2011],
-    [4, 31, 2011, 2013],
-    [4, 31, 2013, 2015],
-    [4, 31, 2015, 2017],
+    [4, '31', 2010, 2010],
+    [4, '31', 2012, 2012],
+    [4, '31', 2014, 2014],
+    [4, '31', 2016, 2016],
+    [4, '31', 2018, 2018],
 
-    [6, 33, 2007, 2009],
-    [6, 33, 2010, 2012],
-    [6, 33, 2013, 2015],
+    [6, '33', 2010, 2012],
+    [6, '33', 2013, 2015],
+    [6, '33', 2016, 2018],
 
-    [6, 42, 2007, 2010],
-    [6, 42, 2010, 2013],
-    [6, 42, 2013, 2016],
+    [6, '42', 2011, 2012],
+    [6, '42', 2014, 2015],
+    [6, '42', 2017, 2018],
 
-    [8, 44, 2007, 2010],
-    [8, 44, 2011, 2014],
+    [8, '44', 2011, 2014],
+    [8, '44', 2015, 2018],
 
-    [8, 53, 2007, 2011],
-    [8, 53, 2011, 2015]
-    
-])
-
-cutByYear_whole(['./주식_rate_1차_전처리.csv'],
-[
-    [2007, 2010], [2009, 2012], [2011, 2014], [2013, 2016], [2015, 2018],
-    [2007, 2012], [2010, 2015], [2013, 2018],
-    [2007, 2014], [2011, 2018]
+    [8, '53', 2012, 2014],
+    [8, '53', 2016, 2018]
 ])
